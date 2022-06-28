a28f3198ee2a7fe5d3d3f937121e021cc48b4e63
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2022-06-28 20:08:11 -0300

    Add support to `container-interop/service-provider` compatible containers

---
+++ src/ContainerInteropServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of coisa/logger.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/factory
+ * @link      https://12factor.net/logs
+ *
+ * @copyright Copyright (c) 2022 Felipe Sayão Lobato Abreu <github@mentor.dev.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace CoiSA\Logger;
+
+use Interop\Container\ServiceProviderInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+final class ContainerInteropServiceProvider implements ServiceProviderInterface
+{
+    public function getFactories(): array
+    {
+        return [
+            'logger'               => $this->createAliasCallableFactory(LoggerInterface::class),
+            LoggerInterface::class => $this->createAliasCallableFactory(Logger::class),
+            Logger::class          => [LoggerFactory::class, 'createLogger'],
+        ];
+    }
+
+    public function getExtensions(): array
+    {
+        return [];
+    }
+
+    private function createAliasCallableFactory(string $alias): callable
+    {
+        return fn (ContainerInterface $container): LoggerInterface => $container->get($alias);
+    }
+}
